693765f43db07de8853349dea5cba6dbd4c235ed
Josh Matthews <josh@joshmatthews.net>
2022-11-17 09:55:10 -0500

    Update wrapper generation to work on mac.

--- rust-mozjs/src/generate_wrappers.sh
+++ rust-mozjs/src/generate_wrappers.sh
@@ -4,23 +4,23 @@ grep_heur() {
     grep -v "link_name" "$1" | \
         grep -v '"\]' | \
         grep -F -v '/\*\*' | \
-        sed -z 's/,\n */, /g' | \
-        sed -z 's/:\n */: /g' | \
-        sed -z 's/\n *->/ ->/g' | \
+        gsed -z 's/,\n */, /g' | \
+        gsed -z 's/:\n */: /g' | \
+        gsed -z 's/\n *->/ ->/g' | \
         grep -v '^\}$' | \
-        sed 's/^ *pub/pub/' | \
-        sed -z 's/\;\n/\n/g' | \
+        gsed 's/^ *pub/pub/' | \
+        gsed -z 's/\;\n/\n/g' | \
         grep 'pub fn' | \
         grep Handle | \
         grep -v roxyHandler | \
         grep -v '\bIdVector\b' | # name clash between rust::IdVector and JS::IdVector \
         grep -v 'pub fn Unbox' | # this function seems to be platform specific \
         grep -v 'CopyAsyncStack' | # arch-specific bindgen output
-        sed 's/root:://g' |
-        sed 's/JS:://g' |
-        sed 's/js:://g' |
-        sed 's/mozilla:://g' |
-        sed 's/Handle<\*mut JSObject>/HandleObject/g'
+        gsed 's/root:://g' |
+        gsed 's/JS:://g' |
+        gsed 's/js:://g' |
+        gsed 's/mozilla:://g' |
+        gsed 's/Handle<\*mut JSObject>/HandleObject/g'
 }
 
 grep_heur target/debug/build/mozjs_sys-*/out/build/jsapi.rs | sed 's/\(.*\)/wrap!(jsapi: \1);/g'  > rust-mozjs/src/jsapi_wrappers.in
