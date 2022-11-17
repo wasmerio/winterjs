1317494a97667e3d8347c1395c5bb14099629e8c
Josh Matthews <josh@joshmatthews.net>
2022-11-17 01:12:30 -0500

    Add missing functions

--- mozjs/src/lib.rs
+++ mozjs/src/lib.rs
@@ -26,3 +26,40 @@ pub use generated::root as jsapi;
 mod generated {
     include!(concat!(env!("OUT_DIR"), "/build/jsapi.rs"));
 }
+
+fn panic_hook(info: &std::panic::PanicInfo) {
+    panic!("{}", info);
+}
+
+/// Configure a panic hook to redirect rust panics to MFBT's MOZ_Crash.
+/// See https://searchfox.org/mozilla-central/rev/2bdaa395cb841b28f8ef74882a61df5efeedb42b/mozglue/static/rust/lib.rs#99-103
+#[no_mangle]
+pub extern "C" fn install_rust_panic_hook() {
+    std::panic::set_hook(Box::new(panic_hook));
+}
+
+#[cfg(feature = "oom_with_hook")]
+mod oom_hook {
+    use std::alloc::{set_alloc_error_hook, Layout};
+
+    extern "C" {
+        fn RustHandleOOM(size: usize) -> !;
+    }
+
+    pub fn hook(layout: Layout) {
+        unsafe {
+            RustHandleOOM(layout.size());
+        }
+    }
+
+    pub fn install() {
+        set_alloc_error_hook(hook);
+    }
+}
+
+// See https://searchfox.org/mozilla-central/rev/2bdaa395cb841b28f8ef74882a61df5efeedb42b/mozglue/static/rust/lib.rs#105-128
+#[no_mangle]
+pub extern "C" fn install_rust_oom_hook() {
+    #[cfg(feature = "oom_with_hook")]
+    oom_hook::install();
+}
