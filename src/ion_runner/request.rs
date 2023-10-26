4a6ae224bd824653f43e18c8a154e42d20006c73
Arshia Ghafoori <arshia001@live.com>
2023-10-26 16:00:04 +0330

    Add Request.formData

--- src/ion_runner/request.rs
+++ src/ion_runner/request.rs
@@ -21,9 +21,11 @@ impl<'cx> ToValue<'cx> for Body {
 
 #[js_class]
 mod class {
-    use ion::{typedarray::ArrayBuffer, Context, Value};
+    use http::header::CONTENT_TYPE;
+    use ion::{typedarray::ArrayBuffer, ClassDefinition, Context, Value};
     use mozjs::jsapi::JSObject;
     use mozjs_sys::jsgc::Heap;
+    use runtime::globals::form_data::FormData;
 
     use super::Body;
 
@@ -107,6 +109,43 @@ mod class {
             }
 
             Ok((*result.to_object(cx)).get())
+        }
+
+        #[ion(name = "formData")]
+        pub async fn form_data(&mut self, cx: &Context<'_>) -> ion::Result<*mut JSObject> {
+            let headers = self.get_headers();
+            let Some(content_type) = headers.get(CONTENT_TYPE.to_string())? else {
+                return Err(ion::Error::new(
+                    "No content-type header, cannot decide form data format",
+                    ion::ErrorKind::Type,
+                ));
+            };
+            let content_type = content_type.to_string();
+
+            let bytes = self.body.take().and_then(|b| b.0);
+
+            let bytes = bytes.as_ref().map(|b| b.as_ref()).unwrap_or(&[][..]);
+
+            if content_type.starts_with("application/x-www-form-urlencoded") {
+                let parsed = form_urlencoded::parse(bytes.as_ref());
+                let mut form_data = FormData::constructor();
+
+                for (key, val) in parsed {
+                    form_data.append_native_string(key.into_owned(), val.into_owned());
+                }
+
+                Ok(FormData::new_object(cx, form_data))
+            } else if content_type.starts_with("multipart/form-data") {
+                Err(ion::Error::new(
+                    "multipart/form-data deserialization is not supported yet",
+                    ion::ErrorKind::Normal,
+                ))
+            } else {
+                Err(ion::Error::new(
+                    "Invalid content-type, cannot decide form data format",
+                    ion::ErrorKind::Type,
+                ))
+            }
         }
     }
 }
