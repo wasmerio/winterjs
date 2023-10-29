36f0591734879d183124ab88cebdb11dfdcf532f
Rudra Arora <aroraru@tcd.ie>
2023-10-29 01:04:46 +0100

    fix: change cargo wasix command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-cargo +wasix3 build --target wasm32-wasmer-wasi -r
+cargo wasix build -r
 mv target/wasm32-wasmer-wasi/release/wasmer-winter.wasm x.wasm
 wasm-opt x.wasm -o target/wasm32-wasmer-wasi/release/wasmer-winter.wasm -O1 --enable-bulk-memory --enable-threads --enable-reference-types --no-validation --asyncify
 rm x.wasm
