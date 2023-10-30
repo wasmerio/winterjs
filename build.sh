c3ff8eb92b6e1b88e32f16fa33100121acb7bff6
Arshia Ghafoori <arshia001@live.com>
2023-10-30 15:24:42 +0330

    Fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-cargo wasix build -r
+# Note: cargo-wasix automatically runs wasm-opt with -O2, which makes the resulting binary unusable.
+# Instead, we use the toolchain to build (cargo +wasix instead of cargo wasix) and optimize manually.
+cargo +wasix build --target wasm32-wasmer-wasi -r
 mv target/wasm32-wasmer-wasi/release/wasmer-winter.wasm x.wasm
 wasm-opt x.wasm -o target/wasm32-wasmer-wasi/release/wasmer-winter.wasm -O1 --enable-bulk-memory --enable-threads --enable-reference-types --no-validation --asyncify
 rm x.wasm
