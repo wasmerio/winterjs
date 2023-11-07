7de17a4a89b1302ea0993cac17aaf6042671ab1a
Rudra Arora <aroraru@tcd.ie>
2023-11-07 19:55:49 +0000

    feat: hook the test suite

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -1,18 +1,70 @@
-use std::ffi::OsString;
+use libtest_mimic::{Arguments, Trial};
+use serde::Deserialize;
 
-#[derive(Debug, Clone)]
-pub struct Test {
-    test_name: String,
-    test_file: OsString,
+// Sample Test Case
+// test_name = "3.7-headers"
+// test_route = "3-headers"
+// expected_output = "Hello, world!"
+// expected_response_status = 200
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TestCase {
+    pub test_name: String,
+    pub test_route: String,
+    pub expected_output: String,
+    pub expected_response_status: u16,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct TestConfig {
-    pub tests: Vec<Test>,
+    pub tests: Vec<TestCase>,
 }
 
 #[derive(Debug)]
 pub struct TestManager {
     rt: tokio::runtime::Runtime,
     config: TestConfig,
+}
+
+impl TestManager {
+    pub fn new(config: TestConfig) -> Result<Self, anyhow::Error> {
+        let rt = tokio::runtime::Runtime::new()?;
+        Ok(TestManager { rt, config })
+    }
+
+    fn collect_tests(&self) -> Result<Vec<Trial>, anyhow::Error> {
+        let mut tests: Vec<Trial> = Vec::new();
+        for test_case in self.config.tests.iter() {
+            let rt = self.rt.handle().clone();
+
+            let test_name = test_case.test_name.clone();
+            let test_route = test_case.test_route.clone();
+            let expected_output = test_case.expected_output.clone();
+            let expected_resp_status = test_case.expected_response_status;
+
+            let test = Trial::test(test_name, move || {
+                let test_route = test_route.clone();
+                let expected_output = expected_output.clone();
+                // let expected_response_status = expected_resp_status;
+                rt.block_on(async move {
+                    let client = reqwest::Client::new();
+                    let url = format!("http://localhost:8000/{}", test_route);
+                    let response = client.get(&url).send().await?;
+                    // let response_status = response.status();
+                    let response_body = response.text().await?;
+                    // assert_eq!(response_status, expected_response_status.try_into()?);
+                    assert_eq!(response_body, expected_output);
+                    anyhow::Result::<()>::Ok(())
+                })?;
+                Ok(())
+            });
+            tests.push(test);
+        }
+        Ok(tests)
+    }
+
+    pub fn run_tests(self, args: &Arguments) -> Result<(), anyhow::Error> {
+        let tests = self.collect_tests()?;
+        libtest_mimic::run(args, tests).exit();
+    }
 }
--- test-suite/tests/integration.rs
+++ test-suite/tests/integration.rs
@@ -1,10 +1,30 @@
+use anyhow::Ok;
 use libtest_mimic::Arguments;
-use test_suite::TestManager;
+use serde::Deserialize;
+use std::fs;
+use test_suite::{TestCase, TestConfig, TestManager};
+use toml;
+
+// Read and parse the TOML file into a TestConfig
+fn read_test_cases() -> Result<TestConfig, anyhow::Error> {
+    let file_path = format!("{}/tests/tests.toml", env!("CARGO_MANIFEST_DIR"));
+    let file_content = fs::read_to_string(file_path).expect("File should be read without problems");
+    let test_config: TestConfig = toml::from_str(&file_content)?;
+    Ok(test_config)
+}
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Arguments::from_args();
 
-    // let mut manager = TestManager::new()?;
-    // manager.run_tests()?;
-    Ok(())
+    // Read the test cases from the TOML file
+    let test_config = read_test_cases()?;
+
+    // add a run test inside the test manager which holds the tokio runtime and runs the tests use a handle and clone the handle
+    TestManager::new(test_config)?
+        .run_tests(&args)
+        //stop the server thread
+        .map(|_| {
+            ()
+            // server_thread.join().unwrap();
+        })
 }
