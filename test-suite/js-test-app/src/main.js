d86dc05e92c787041fc3494a937f8aa58dda727e
Rudra Arora <aroraru@tcd.ie>
2023-11-07 18:05:57 +0000

    fix: route handling

--- test-suite/js-test-app/src/main.js
+++ test-suite/js-test-app/src/main.js
@@ -14,29 +14,34 @@ function router(req) {
   const url = new URL(req.url);
   const path = url.pathname;
 
-  console.log(path.startsWith("/1-hello"));
-  switch (path) {
-    case "/1-hello":
-      return handleHello1(req);
-    case "/2-blob":
-      return handleBlob2(req);
-    case "/3-headers":
-      return handleFormData3(req);
-    case "/4-request":
-      return handleRequest4(req);
-    case "/5-response":
-      return handleResponse5(req);
-    case "/6-text-encoder":
-      return handleTextEncoder6(req);
-    case "/7-text-decoder":
-      return handleTextDecoder7(req);
-    case "/8-url":
-      return handleURL8(req);
-    case "/10-atob-btoa":
-      return handleAtobBtoA9(req);
-    default:
-      return new Response("Route Not Found", { status: 404 });
+  if (path.startsWith("/1-hello")) {
+    return handleHello1(req);
   }
+  if (path.startsWith("/2-blob")) {
+    return handleBlob2(req);
+  }
+  if (path.startsWith("/3-headers")) {
+    return handleFormData3(req);
+  }
+  if (path.startsWith("/4-request")) {
+    return handleRequest4(req);
+  }
+  if (path.startsWith("/5-response")) {
+    return handleResponse5(req);
+  }
+  if (path.startsWith("/6-text-encoder")) {
+    return handleTextEncoder6(req);
+  }
+  if (path.startsWith("/7-text-decoder")) {
+    return handleTextDecoder7(req);
+  }
+  if (path.startsWith("/8-url")) {
+    return handleURL8(req);
+  }
+  if (path.startsWith("/10-atob-btoa")) {
+    return handleAtobBtoA9(req);
+  }
+  return new Response("Route Not Found", { status: 404 });
 }
 
 addEventListener("fetch", (fetchEvent) => {
