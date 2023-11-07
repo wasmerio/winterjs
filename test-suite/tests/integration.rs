dd4d40cb5979efa6e3982fdcebfee3400bf6adde
Rudra Arora <aroraru@tcd.ie>
2023-11-07 13:47:33 +0000

    add: tests and integration test file

---
+++ test-suite/tests/integration.rs
@@ -0,0 +1,10 @@
+use libtest_mimic::Arguments;
+use test_suite::TestManager;
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Arguments::from_args();
+
+    // let mut manager = TestManager::new()?;
+    // manager.run_tests()?;
+    Ok(())
+}
