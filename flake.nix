8592d539584636a6fc9cf24b5ff57963c709bbbb
Christoph Herzog <chris@theduke.at>
2023-11-09 09:55:35 +0100

    build: Nix devshell improvements
    Add additional dependencies to the Nix devshell.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "app";
+  description = "winterjs - A Javascript runtime following the WinterCG spec.";
 
   inputs = {
     flakeutils = {
@@ -11,13 +11,14 @@
   outputs = { self, nixpkgs, flakeutils }:
     flakeutils.lib.eachDefaultSystem (system:
       let
-        NAME = "app";
-        VERSION = "0.1";
+        NAME = "winterjs";
+        VERSION = "0.1.0";
 
         pkgs = import nixpkgs {
           inherit system;
         };
 
+        llvm = pkgs.llvmPackages_latest;
       in
       rec {
 
@@ -36,35 +37,67 @@
         # };
         # defaultApp = apps.${NAME};
 
-        devShell = pkgs.stdenv.mkDerivation {
-          name = NAME;
-          src = self;
-          buildInputs = with pkgs; [
-            # rustup
-            # python3
-            # perl
-
-            llvmPackages_16.bintools-unwrapped
+        devShell = pkgs.mkShell.override { stdenv = pkgs.llvmPackages_latest.stdenv; } rec {
+          packages = with pkgs; [
+            # llvmPackages_16.bintools-unwrapped
             pkg-config
             gnum4
 
+            # builder
+            gnumake
+            cmake
+            bear
+
+            # debugger
+            llvmPackages_latest.lldb
+            gdb
+
+            # fix headers not found
+            clang-tools
+
+            # LSP and compiler
+            llvmPackages_latest.libstdcxxClang
+
+            # other tools
+            cppcheck
+            llvmPackages_latest.libllvm
+            valgrind
+
+            # stdlib for cpp
+            llvmPackages_latest.libcxx
+              
+            # libs
+            llvmPackages_latest.libclang
             zlib
-            llvmPackages_16.libclang
           ];
-          runtimeDependencies = with pkgs; [ ];
 
-          LD_LIBRARY_PATH= pkgs.lib.makeLibraryPath (with pkgs; [
-            zlib
-            llvmPackages_16.libclang
-          ]);
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
 
-          # standalone as(1) doesn’t treat -DNDEBUG as -D NDEBUG (define), but rather -D (produce
-          # assembler debugging messages) + -N (invalid option); see also <https://bugs.gentoo.org/732190>
-          # /nix/store/a64w6zy8w9hcj6b4g5nz0dl6zyd24c1x-gcc-wrapper-11.3.0/bin/as: invalid option -- 'N'
-          # make[4]: *** [/path/to/mozjs/mozjs/mozjs/config/rules.mk:664: icu_data.o] Error 1
-          # make[3]: *** [/path/to/mozjs/mozjs/mozjs/config/recurse.mk:72: config/external/icu/data/target-objects] Error 2
-          AS="$CC -c";
+
+          shellHook = ''
+            export AS="$CC -c"
+          '';
         };
+
+        # devShell = pkgs.stdenv.mkDerivation {
+        #   name = NAME;
+        #   src = self;
+        #   buildInputs = with pkgs; [
+        #   ];
+        #   runtimeDependencies = with pkgs; [ ];
+
+        #   LD_LIBRARY_PATH= pkgs.lib.makeLibraryPath (with pkgs; [
+        #     zlib
+        #     llvmPackages_16.libclang
+        #   ]);
+
+        #   # standalone as(1) doesn’t treat -DNDEBUG as -D NDEBUG (define), but rather -D (produce
+        #   # assembler debugging messages) + -N (invalid option); see also <https://bugs.gentoo.org/732190>
+        #   # /nix/store/a64w6zy8w9hcj6b4g5nz0dl6zyd24c1x-gcc-wrapper-11.3.0/bin/as: invalid option -- 'N'
+        #   # make[4]: *** [/path/to/mozjs/mozjs/mozjs/config/rules.mk:664: icu_data.o] Error 1
+        #   # make[3]: *** [/path/to/mozjs/mozjs/mozjs/config/recurse.mk:72: config/external/icu/data/target-objects] Error 2
+        #   AS="$CC -c";
+        # };
       }
     );
 }
