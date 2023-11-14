3e5bd610a686783dbf11caec7bf8602f2c1de9cc
Rudra Arora <aroraru@tcd.ie>
2023-11-14 13:55:54 +0000

    add: temp js files from wrangler

---
+++ test-suite/.wrangler/tmp/bundle-YQWXdC/middleware-insertion-facade.js
@@ -0,0 +1,3 @@
+import { __facade_registerInternal__ } from "/Users/xorcist/.nvm/versions/node/v20.7.0/lib/node_modules/wrangler/templates/middleware/loader-sw.ts";
+import * as __MIDDLEWARE_0__ from "/Users/xorcist/.nvm/versions/node/v20.7.0/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+__facade_registerInternal__([__MIDDLEWARE_0__.default])
