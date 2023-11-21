9ac03beb33f8df88518f3291c8e3c5c567b4f087
Rudra Arora <aroraru@tcd.ie>
2023-11-21 11:12:06 +0000

    removed redundant files

--- test-suite/.wrangler/tmp/bundle-YQWXdC/middleware-insertion-facade.js
+++
@@ -1,3 +0,0 @@
-import { __facade_registerInternal__ } from "/Users/xorcist/.nvm/versions/node/v20.7.0/lib/node_modules/wrangler/templates/middleware/loader-sw.ts";
-import * as __MIDDLEWARE_0__ from "/Users/xorcist/.nvm/versions/node/v20.7.0/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
-__facade_registerInternal__([__MIDDLEWARE_0__.default])
