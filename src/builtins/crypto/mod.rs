ae8c4745c7ae5136197239f42dcc12924280755e
Arshia Ghafoori <arshia001@live.com>
2024-01-18 13:34:54 +0330

    Make getRandomValues spec-compliant

--- src/builtins/crypto/mod.rs
+++ src/builtins/crypto/mod.rs
@@ -1,20 +1,43 @@
 mod subtle;
 
-use ion::{conversions::ToValue, function_spec, ClassDefinition, Context, Object};
-use mozjs::typedarray::ArrayBufferView;
-use mozjs_sys::jsapi::{JSFunctionSpec, JSObject};
+use ion::{
+    conversions::ToValue, function_spec, ClassDefinition, Context, Error, ErrorKind, Object, Result,
+};
+use mozjs::{
+    jsapi::{DataView_ClassPtr, UnwrapFloat32Array, UnwrapFloat64Array},
+    typedarray::ArrayBufferView,
+};
+use mozjs_sys::jsapi::{JSFunctionSpec, JSObject, JS_InstanceOf};
 use rand::RngCore;
 use runtime::modules::NativeModule;
 
 #[js_fn]
-fn get_random_values(array: ArrayBufferView) -> *mut JSObject {
+fn get_random_values(cx: &Context, array: ArrayBufferView) -> Result<*mut JSObject> {
+    if array.len() > 65536 {
+        return Err(Error::new("Quota exceeded", ErrorKind::Normal));
+    }
+    unsafe {
+        let rooted = cx.root_object(*array.underlying_object());
+        if !UnwrapFloat32Array(*array.underlying_object()).is_null()
+            || !UnwrapFloat64Array(*array.underlying_object()).is_null()
+            || JS_InstanceOf(
+                cx.as_ptr(),
+                rooted.handle().into(),
+                DataView_ClassPtr,
+                std::ptr::null_mut(),
+            )
+        {
+            return Err(Error::new("Bad array element type", ErrorKind::Type));
+        }
+    }
+
     let mut array = array;
     let slice = unsafe { array.as_mut_slice() };
     rand::thread_rng().fill_bytes(slice);
 
     // We have to call underlying_object because ToValue is not
     // implemented for ArrayBufferView
-    unsafe { *array.underlying_object() }
+    Ok(unsafe { *array.underlying_object() })
 }
 
 #[js_fn]
