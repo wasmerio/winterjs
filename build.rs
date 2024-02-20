43a294278c9000f0b9e9471c54077a3da40b6cae
Arshia Ghafoori <arshia001@live.com>
2024-02-20 22:48:01 +0330

    Run `npx tsc` on debug builds only

--- build.rs
+++ build.rs
@@ -1,13 +1,20 @@
 use std::process::Command;
 
 fn main() {
-    let mut dir = std::env::current_dir().unwrap();
-    dir.extend(["src", "builtins", "internal_js_modules"]);
-    assert!(Command::new("npx")
-        .arg("tsc")
-        .current_dir(dir)
-        .output()
-        .unwrap()
-        .status
-        .success());
+    // We want to let people install WinterJS from source, so we can't have
+    // a dependency on TSC at all times. The assumption here is that whoever
+    // updates TS sources for builtin modules will at least run WinterJS
+    // once in debug mode, and the JS scripts will be updated and pushed.
+    let profile = std::env::var("PROFILE").unwrap();
+    if profile == "debug" {
+        let mut dir = std::env::current_dir().unwrap();
+        dir.extend(["src", "builtins", "internal_js_modules"]);
+        assert!(Command::new("npx")
+            .arg("tsc")
+            .current_dir(dir)
+            .output()
+            .unwrap()
+            .status
+            .success());
+    }
 }
