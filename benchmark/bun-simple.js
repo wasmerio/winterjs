08fe49f562d05acea4cb4612d0dcfb3e155a9c32
Syrus Akbary <me@syrusakbary.com>
2024-02-20 12:36:38 -0800

    Update bun-simple.js

--- benchmark/bun-simple.js
+++ benchmark/bun-simple.js
@@ -1,9 +1,8 @@
 const server = Bun.serve({
-    port: 8080,
-    fetch(request) {
-      return new Response('hello');
-    },
-  });
+  port: 8080,
+  fetch(request) {
+    return new Response('hello');
+  },
+});
   
-  console.log(`Server running at ${server.url}`);
-  
+console.log(`Server running at ${server.url}`);
