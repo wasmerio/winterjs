70b0d3c44020a53202efde1908ff96ef6c8cba4c
Arshia Ghafoori <arshia001@live.com>
2024-03-05 12:21:43 +0330

    Move engine params from test config file to CLI args

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use anyhow::{bail, Result};
 use futures::{stream::FuturesUnordered, StreamExt};
 use libtest_mimic::{Arguments, Failed, Trial};
@@ -20,139 +22,75 @@ pub struct TestCase {
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub enum Engine {
-    WinterJS,
-    Wrangler,
-    WorkerD,
-}
-
-impl Engine {
-    pub fn port(&self) -> u16 {
-        match self {
-            Self::WinterJS => 8080,
-            Self::Wrangler => 8787,
-            Self::WorkerD => 8789,
-        }
-    }
-
-    pub fn name(&self) -> String {
-        match self {
-            Self::WinterJS => "WinterJS".to_string(),
-            Self::Wrangler => "Wrangler".to_string(),
-            Self::WorkerD => "workerd".to_string(),
-        }
-    }
-
-    pub fn must_pass(&self) -> bool {
-        match self {
-            // We mostly don't care if workerd and wrangler pass our tests,
-            // since the implementations aren't perfect. If they do, we log
-            // the failures as a warning that the test *may* be broken, but
-            // don't fail the entire test process otherwise.
-            Self::WinterJS => true,
-            Self::WorkerD | Self::Wrangler => false,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct EngineConfig {
-    pub engine: Engine,
-
-    // The number of seconds to run a stress test for. If this
-    // is zero, stress-testing will be disabled.
-    // All requests to the engine must pass for a stress-test
-    // to be considered successful.
-    pub stress_duration: Option<f64>,
-
-    // The number of concurrent connections to open.
-    pub max_concurrent_connections: Option<usize>,
-
-    // The minimum number of connections that have to be made
-    // for the stress test to be considered successful. The test
-    // will not be stopped if this number of connections haven't
-    // been made once the duration is up.
-    pub min_connections: Option<usize>,
-
-    // The maximum number of connections that can be made. Some
-    // times, when making too many connections, the OS starts
-    // rejecting more connections, so we limit the total number
-    // of connections.
-    pub max_connections: Option<usize>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
 pub struct TestConfig {
-    #[serde(rename = "engine")]
-    pub engines: Vec<EngineConfig>,
-
     #[serde(rename = "test_case")]
     pub test_cases: Vec<TestCase>,
+}
+
+#[derive(Clone, Debug)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: u16,
+    pub critical: bool,
+}
+
+#[derive(Clone, Debug)]
+pub struct StressTestConfig {
+    pub duration: Duration,
+    pub max_concurrent_connections: usize,
+    pub min_connections: usize,
+    pub max_connections: Option<usize>,
 }
 
 #[derive(Debug)]
 pub struct TestManager {
     rt: tokio::runtime::Runtime,
-    config: TestConfig,
-}
-
-struct Tests {
-    must_pass: Vec<Trial>,
-    others: Vec<Trial>,
+    test_config: TestConfig,
+    server_config: ServerConfig,
+    stress_config: Option<StressTestConfig>,
 }
 
 impl TestManager {
-    pub fn new(config: TestConfig) -> Result<Self, anyhow::Error> {
+    pub fn new(
+        test_config: TestConfig,
+        server_config: ServerConfig,
+        stress_config: Option<StressTestConfig>,
+    ) -> Result<Self, anyhow::Error> {
         let rt = tokio::runtime::Runtime::new()?;
-        Ok(TestManager { rt, config })
+        Ok(TestManager {
+            rt,
+            test_config,
+            server_config,
+            stress_config,
+        })
     }
 
-    fn collect_tests(&self) -> Result<Tests, anyhow::Error> {
-        let mut tests = Tests {
-            must_pass: vec![],
-            others: vec![],
-        };
+    fn collect_tests(&self) -> Result<Vec<Trial>, anyhow::Error> {
+        let mut tests = vec![];
 
-        for engine_config in self.config.engines.iter() {
-            let test_collection = if engine_config.engine.must_pass() {
-                &mut tests.must_pass
-            } else {
-                &mut tests.others
+        for test_case in self.test_config.test_cases.iter() {
+            let test_name = test_case.test_name.clone();
+
+            if test_case.skip.is_some() {
+                println!("Skipping {test_name}");
+                continue;
+            }
+
+            let rt = self.rt.handle().clone();
+            let test_case = test_case.clone();
+
+            let server_config = self.server_config.clone();
+            let test = match self.stress_config {
+                Some(ref stress_config) => {
+                    let stress_config = stress_config.clone();
+                    Trial::test(test_name, move || {
+                        stress_test(rt, server_config, stress_config, test_case)
+                    })
+                }
+                None => Trial::test(test_name, move || test_once(rt, server_config, test_case)),
             };
 
-            for test_case in self.config.test_cases.iter() {
-                let test_name = format!(
-                    "{}{}:{}",
-                    engine_config.engine.name(),
-                    if engine_config.stress_duration.is_some() {
-                        "-stress"
-                    } else {
-                        ""
-                    },
-                    test_case.test_name
-                );
-
-                if test_case.skip.is_some() {
-                    println!("Skipping {test_name}");
-                    continue;
-                }
-
-                let rt = self.rt.handle().clone();
-                let test_case = test_case.clone();
-
-                let test = if engine_config.stress_duration.is_some() {
-                    let engine_config = engine_config.clone();
-                    let test_name_clone = test_name.clone();
-                    Trial::test(test_name, move || {
-                        stress_test(rt, engine_config, test_case, test_name_clone)
-                    })
-                } else {
-                    let engine = engine_config.engine.clone();
-                    Trial::test(test_name, move || test_once(rt, engine, test_case))
-                };
-
-                test_collection.push(test);
-            }
+            tests.push(test);
         }
 
         Ok(tests)
@@ -160,26 +98,21 @@ impl TestManager {
 
     pub fn run_tests(self, mut args: Arguments) -> Result<(), anyhow::Error> {
         let tests = self.collect_tests()?;
-        if self
-            .config
-            .engines
-            .iter()
-            .any(|e| e.stress_duration.is_some())
-        {
+        if self.stress_config.is_some() {
             // Use a single thread for stress tests, since the many concurrent
             // connections can exhaust available ports and cause failures
             args.test_threads = Some(1);
         }
-        let conclusion = libtest_mimic::run(&args, tests.others);
+        let conclusion = libtest_mimic::run(&args, tests);
         if conclusion.has_failed() {
-            println!(
-                "Warning: Some tests failed on other engines, check \
-                test output above to make sure the test cases are correct"
-            );
-        }
-
-        if libtest_mimic::run(&args, tests.must_pass).has_failed() {
-            bail!("Some tests failed")
+            if self.server_config.critical {
+                bail!("Some tests failed")
+            } else {
+                println!(
+                    "Warning: Some tests failed, check test output above to \
+                    make sure the test cases are correct"
+                );
+            }
         }
 
         Ok(())
@@ -188,48 +121,37 @@ impl TestManager {
 
 fn stress_test(
     rt: tokio::runtime::Handle,
-    config: EngineConfig,
+    server_config: ServerConfig,
+    stress_config: StressTestConfig,
     test_case: TestCase,
-    test_name: String,
 ) -> Result<(), Failed> {
     rt.block_on(async move {
         let client = reqwest::ClientBuilder::new()
             .pool_max_idle_per_host(100)
             .build()?;
 
-        let duration = config
-            .stress_duration
-            .expect("stress_test must be called with an engine config that has a stress duration");
-        if duration <= 0.0 {
-            return Err("Stress test duration must be greater than zero".into());
-        }
-
-        let min_connections = config.min_connections.unwrap_or_default();
-        let Some(max_concurrent_connections) = config.max_concurrent_connections else {
-            return Err("max_concurrent_connections must be specified for all stress tests".into());
-        };
-
         let mut futures = FuturesUnordered::new();
 
         let mut error = None;
         let mut connections = 0;
 
-        let duration = std::time::Duration::from_secs_f64(duration);
         let start_instant = std::time::Instant::now();
 
         loop {
-            if start_instant.elapsed() >= duration && connections >= min_connections {
+            if start_instant.elapsed() >= stress_config.duration
+                && connections >= stress_config.min_connections
+            {
                 break;
             }
 
-            if let Some(max_connections) = config.max_connections {
+            if let Some(max_connections) = stress_config.max_connections {
                 if connections >= max_connections {
                     break;
                 }
             }
 
-            while futures.len() < max_concurrent_connections {
-                futures.push(run_test_once(&config.engine, &test_case, &client));
+            while futures.len() < stress_config.max_concurrent_connections {
+                futures.push(run_test_once(&server_config, &test_case, &client));
                 connections += 1;
             }
 
@@ -252,11 +174,17 @@ fn stress_test(
 
         match error {
             None => {
-                println!("Test {test_name} - {connections} successful connections");
+                println!(
+                    "Test {} - {connections} successful connections",
+                    test_case.test_name
+                );
                 Ok(())
             }
             Some(e) => {
-                println!("Test {test_name} - error after {connections} connections");
+                println!(
+                    "Test {} - error after {connections} connections",
+                    test_case.test_name
+                );
                 Err(e)
             }
         }
@@ -265,24 +193,26 @@ fn stress_test(
 
 fn test_once(
     rt: tokio::runtime::Handle,
-    engine: Engine,
+    server_config: ServerConfig,
     test_case: TestCase,
 ) -> Result<(), Failed> {
     let client = reqwest::Client::new();
-    rt.block_on(run_test_once(&engine, &test_case, &client))?;
+    rt.block_on(run_test_once(&server_config, &test_case, &client))?;
     Ok(())
 }
 
 async fn run_test_once(
-    engine: &Engine,
+    server_config: &ServerConfig,
     test_case: &TestCase,
     client: &reqwest::Client,
 ) -> Result<()> {
-    let runner_port = engine.port();
     let expected_response_status =
         StatusCode::from_u16(test_case.expected_response_status).expect("Invalid status code");
 
-    let url = format!("http://localhost:{}/{}", runner_port, test_case.test_route);
+    let url = format!(
+        "http://{}:{}/{}",
+        server_config.host, server_config.port, test_case.test_route
+    );
     let mut request = client.get(&url);
     if let Some(timeout) = test_case.timeout {
         request = request.timeout(std::time::Duration::from_secs_f64(timeout));
--- test-suite/src/main.rs
+++ test-suite/src/main.rs
@@ -1,15 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
-use test_suite::{TestConfig, TestManager};
+use test_suite::{ServerConfig, StressTestConfig, TestConfig, TestManager};
 
-use std::{
-    fs,
-    net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream},
-    path::Path,
-    time::Duration,
-};
+use std::{fs, path::Path, time::Duration};
 
-// Read and parse the TOML file into a TestConfig
 fn read_test_cases(test_definition_file_path: Option<impl AsRef<Path>>) -> Result<TestConfig> {
     let file_content = fs::read_to_string(
         test_definition_file_path
@@ -22,23 +16,47 @@ fn read_test_cases(test_definition_file_path: Option<impl AsRef<Path>>) -> Resul
     Ok(test_config)
 }
 
-fn ensure_ports_open(ports: Vec<u16>) {
-    let ip = Ipv4Addr::new(127, 0, 0, 1);
-
-    for port in ports {
-        let sock_addr = SocketAddr::new(SocketAddr::V4(SocketAddrV4::new(ip, port)).ip(), port);
-        match TcpStream::connect_timeout(&sock_addr, Duration::from_secs(10)) {
-            Ok(_) => (),
-            Err(_) => panic!("Port {port} is not open, can't continue"),
-        }
-    }
-}
-
 #[derive(clap::Parser)]
 struct TestSuiteArguments {
     /// Path to the test definition file. Defaults to './winterjs-tests.toml'
-    #[arg(short = 'c', long = "test-config-path")]
+    #[arg(short = 'c', long)]
     test_config_path: Option<String>,
+
+    /// Defaults to localhost
+    #[arg(long)]
+    host: Option<String>,
+
+    /// Defaults to 8080
+    #[arg(long)]
+    port: Option<u16>,
+
+    /// If specified, exit code will be 0 regardless of whether tests succeed.
+    /// Useful for running test suite against other engines
+    #[arg(long)]
+    not_critical: bool,
+
+    /// The number of seconds to run a stress test for. If left out, each test
+    /// will be run once only.
+    #[arg(long)]
+    stress_duration: Option<u32>,
+
+    /// Maximum number of concurrent connections when stress testing.
+    /// Defaults to 100.
+    #[arg(long)]
+    stress_max_concurrent_connections: Option<usize>,
+
+    /// The minimum number of connections that have to be made
+    /// for the stress test to be considered successful. The test
+    /// will not be stopped if this number of connections haven't
+    /// been made once the duration is up.
+    #[arg(long)]
+    stress_min_connections: Option<usize>,
+
+    /// The maximum number of connections that can be made. The test
+    /// will be considered successful if this many connections are
+    /// made regardless of whether the duration is up.
+    #[arg(long)]
+    stress_max_connections: Option<usize>,
 
     #[clap(flatten)]
     test_args: libtest_mimic::Arguments,
@@ -47,18 +65,18 @@ struct TestSuiteArguments {
 fn main() -> Result<()> {
     let args = TestSuiteArguments::parse();
 
-    // Read the test cases from the TOML file
     let test_config = read_test_cases(args.test_config_path)?;
+    let server_config = ServerConfig {
+        host: args.host.unwrap_or_else(|| "localhost".to_string()),
+        port: args.port.unwrap_or(8080),
+        critical: !args.not_critical,
+    };
+    let stress_config = args.stress_duration.map(|d| StressTestConfig {
+        duration: Duration::from_secs(d as u64),
+        max_concurrent_connections: args.stress_max_concurrent_connections.unwrap_or(100),
+        min_connections: args.stress_min_connections.unwrap_or(0),
+        max_connections: args.stress_max_connections,
+    });
 
-    let ports: Vec<u16> = test_config
-        .engines
-        .iter()
-        .map(|runner| runner.engine.port())
-        .collect();
-
-    std::thread::sleep(Duration::from_secs(3));
-    ensure_ports_open(ports);
-
-    // add a run test inside the test manager which holds the tokio runtime and runs the tests use a handle and clone the handle
-    TestManager::new(test_config)?.run_tests(args.test_args)
+    TestManager::new(test_config, server_config, stress_config)?.run_tests(args.test_args)
 }
