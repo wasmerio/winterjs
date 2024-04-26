a3267643b0d57a35256af7e1f5c8439ad844b97a
Masato <46220963+MasatoDev@users.noreply.github.com>
2024-04-27 02:25:00 +0900

    Add Event and minimal EventTarget (#76)
    * feat: implements event and eventTarget
    
    * refactor: fix spelling inconsistencies for event test
    
    * fix: remove unneccesary code
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-27-237.us-west-2.compute.internal>

---
+++ test-suite/js-test-app/src/test-files/18-event.js
@@ -0,0 +1,83 @@
+import { Event, EventTarget } from "node:event";
+import { assert_equals } from "../test-utils";
+
+async function handleRequest(request) {
+  try {
+    const TEST_EVENT1 = "testEvent1";
+    const TEST_EVENT2 = "testEvent2";
+
+    const target = new EventTarget();
+    const testEvent1 = new Event(TEST_EVENT1);
+    const testEvent2 = new Event(TEST_EVENT2);
+
+    let testEvent1CalledCount = 0;
+    const testEvent1CallBack = () => {
+      console.log("test event 1 callback is called");
+      testEvent1CalledCount++;
+    };
+
+    let testEvent2CalledCount = 0;
+    const testEvent2CallBack = () => {
+      console.log("test event 2 callback is called");
+      testEvent2CalledCount++;
+    };
+
+    target.addEventListener(TEST_EVENT1, testEvent1CallBack);
+    target.addEventListener(TEST_EVENT2, testEvent2CallBack);
+
+    // Check if dispatch can be executed successfully
+    target.dispatchEvent(testEvent1);
+    assert_equals(
+      testEvent1CalledCount,
+      1,
+      "test event 1 callback was not called"
+    );
+
+    // Check if dispatch isn't executed when other event target was executed
+    assert_equals(
+      testEvent2CalledCount,
+      0,
+      "test event 2 callback was unexpectedly called"
+    );
+
+    // Check if dispatch can be executed successfully
+    target.dispatchEvent(testEvent2);
+    assert_equals(
+      testEvent2CalledCount,
+      1,
+      "test event 2 callback was not called"
+    );
+
+    // Check if dispatch isn't executed when eventListener was removed
+    target.removeEventListener(TEST_EVENT1, testEvent1CallBack);
+    target.dispatchEvent(testEvent1);
+    assert_equals(
+      testEvent1CalledCount,
+      1,
+      "test event 1 callback was unexpectedly called"
+    );
+
+    // Check if dispatch can be executed successfully when other eventListener was remove
+    target.dispatchEvent(testEvent2);
+    assert_equals(
+      testEvent2CalledCount,
+      2,
+      "test event 2 callback was not called"
+    );
+
+    // Check if dispatch isn't executed when eventListener was removed
+    target.removeEventListener(TEST_EVENT2, testEvent2CallBack);
+    target.dispatchEvent(testEvent2);
+    assert_equals(
+      testEvent2CalledCount,
+      2,
+      "test event 2 callback was unexpectedly called"
+    );
+
+    return new Response("All tests passed!");
+  } catch (error) {
+    return new Response(error.message, { status: 500 });
+  }
+}
+
+export { handleRequest };
