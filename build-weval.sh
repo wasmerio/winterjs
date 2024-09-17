6e2b9138fa7e2fb9db13ad4a974b7e630d3b863c
Arshia Ghafoori <arshia001@live.com>
2024-09-17 18:48:07 +0400

    Fine-tune deps and scripts

--- build-weval.sh
+++ build-weval.sh
@@ -1,20 +1,20 @@
-#! /bin/sh
+#! /bin/bash
 
 set -euo pipefail
 set -x
 
 # Note: cargo-wasix automatically runs wasm-opt with -O2, which makes the resulting binary unusable.
 # Instead, we use the toolchain to build (cargo +wasix instead of cargo wasix) and optimize manually.
-cargo +wasix build --target wasm32-wasmer-wasi -r -F weval
+cargo +wasix build --target wasm32-wasmer-wasi -r -F weval $@
 mv target/wasm32-wasmer-wasi/release/winterjs.wasm x.wasm
 # In single-thread-only builds, we skip --asyncify
-wasm-opt x.wasm -o target/wasm32-wasmer-wasi/release/winterjs-st.wasm -O1 --enable-bulk-memory --enable-reference-types --no-validation
+wasm-opt x.wasm -o target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm -O1 --enable-bulk-memory --enable-reference-types --no-validation
 rm x.wasm
-wasm-strip target/wasm32-wasmer-wasi/release/winterjs-st.wasm
+wasm-strip target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm
 
 ../wizex/target/release/wizex \
-    target/wasm32-wasmer-wasi/release/winterjs-st.wasm \
-    -o w-wize.wasm \
+    target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm \
+    -o target/wasm32-wasmer-wasi/release/winterjs-wevalable-wizened.wasm \
     -r _start=wizex.resume \
     --allow-wasix \
     --inherit-stdio true \
@@ -23,6 +23,5 @@ wasm-strip target/wasm32-wasmer-wasi/release/winterjs-st.wasm
     --preload weval=../wevalx/lib/weval-stubs.wat
 
 ../wevalx/target/release/wevalx weval \
-    -i w-wize.wasm \
-    -o w-weav.wasm \
-    &> w-output.log
+    -i target/wasm32-wasmer-wasi/release/winterjs-wevalable-wizened.wasm \
+    -o target/wasm32-wasmer-wasi/release/winterjs-wevalable-wevaled.wasm
