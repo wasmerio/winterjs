f35226eb924bdc4c79d9538cca51aad9595ca286
Arshia Ghafoori <arshia001@live.com>
2024-09-25 15:07:10 +0000

    update stuff

--- build-weval.sh
+++ build-weval.sh
@@ -5,20 +5,17 @@ set -x
 
 # Note: cargo-wasix automatically runs wasm-opt with -O2, which makes the resulting binary unusable.
 # Instead, we use the toolchain to build (cargo +wasix instead of cargo wasix) and optimize manually.
-cargo +wasix build --target wasm32-wasmer-wasi -F weval $@
-mv target/wasm32-wasmer-wasi/debug/winterjs.wasm target/wasm32-wasmer-wasi/debug/winterjs-wevalable.wasm
-# mv target/wasm32-wasmer-wasi/debug/winterjs.wasm x.wasm
+cargo +wasix build --target wasm32-wasmer-wasi -r -F weval $@
+mv target/wasm32-wasmer-wasi/release/winterjs.wasm target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm
 # In single-thread-only builds, we skip --asyncify
-# echo "Optimizing with wasm-opt"
-# wasm-opt x.wasm -o target/wasm32-wasmer-wasi/debug/winterjs-wevalable.wasm -O1 --enable-bulk-memory --enable-reference-types --no-validation
+# wasm-opt x.wasm -o target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm -O1 --enable-bulk-memory --enable-reference-types --no-validation
 # rm x.wasm
-# echo "Optimizing with wasm-strip"
-# wasm-strip target/wasm32-wasmer-wasi/debug/winterjs-wevalable.wasm
+# wasm-strip target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm
 
 echo "Wizening module"
 ../wizex/target/release/wizex \
-    target/wasm32-wasmer-wasi/debug/winterjs-wevalable.wasm \
-    -o w-wize.wasm \
+    target/wasm32-wasmer-wasi/release/winterjs-wevalable.wasm \
+    -o target/wasm32-wasmer-wasi/release/winterjs-wevalable-wizened.wasm \
     -r _start=wizex.resume \
     --allow-wasix \
     --inherit-stdio true \
@@ -28,6 +25,5 @@ echo "Wizening module"
 
 echo "Pre-evaluating module"
 ../wevalx/target/release/wevalx weval \
-    -i w-wize.wasm \
-    -o w-weav.wasm \
-    &> w-output.log
+    -i target/wasm32-wasmer-wasi/release/winterjs-wevalable-wizened.wasm \
+    -o target/wasm32-wasmer-wasi/release/winterjs-wevalable-wevaled.wasm
